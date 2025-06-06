37480926789a3e806d1e160f049e4b654a8bd8d1
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-06 12:10:56 +0530

    day1 task updated

---
+++ Day1_task/Day1_sample.java
@@ -0,0 +1,13 @@
+package day_1;
+
+public class Day1_sample {
+	public static void main (String[] args) {
+		int[] a= {12,3,4,5,6,7};
+		System.out.println(a[3]);
+		for(int i=0;i<4;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
---
+++ Day1_task/task_1.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class task_1 {
+	public static void main(String[] args) {
+		System.out.println(" Task one completed");
+	}
+	
+
+}
