3aaf46ecb02d666d40dcd20bf7195eb035bdb83d
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-19 14:18:01 +0530

    updated

---
+++ day10_task/task1.sql
@@ -0,0 +1,6 @@
+create database kgcas;
+use kgcas;
+create table student(name varchar(32),dept varchar(5),sec char(1), marks int);
+select * from student;
+insert into student values("priya","ct","B",78),("priya","ct","B",54),("sam","ct","A",80),("Harshini","ct","A",35),("Maya","ct","B",25);
+select * from student;
---
+++ day_11/task3.java
@@ -0,0 +1,22 @@
+package day_11;
+import java.sql.*;
+public class task3 {
+	    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	    	
+		        Class.forName("com.mysql.cj.jdbc.Driver");
+		        String url = "jdbc:mysql://localhost:3306/test";
+		        String user = "root";
+		        String pass = "";
+		        
+		        Connection c = DriverManager.getConnection(url, user, pass);
+		        Statement stmt = c.createStatement();
+		        
+		        String query = "alter table ct drop age";
+		        int no=stmt.executeUpdate(query);
+		       
+		        System.out.println("Execution Successfull!");
+		        
+		        stmt.close();
+		        c.close();
+	    }
+	}
